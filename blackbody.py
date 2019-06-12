fa6b09df3edf1044425d841d7f7d360090a99491
Olivia Young <ory001@bowser.phys.wvu.edu>
2019-06-12 00:11:21 -0400

    Add existing files

--- blackbody.py
+++ blackbody.py
@@ -19,29 +19,36 @@ k = 1.38e-23 #kg*m**2/K*s**2
 
 lfuv = 1.516e-7 #m
 
+
 #NUV effective  wavelength
 
 lnuv = 2.267e-7 #m
 
+
 #temperature range
 
-t = np.array([5000.0, 5500.0, 6000.0, 6500.0, 7000.0, 7500.0, 8000.0, 8500.0, 9000.0, 9500.0, 10000.0, 15000.0])
+x = int(input("Enter lower tempture limit: "))
+y = int(input("Enter upper tempture limit: "))
+z = int(input("Enter number of points: "))
+
+t = np.linspace( x, y, num=z , endpoint=True)
+#t = np.array([5000.0, 5500.0, 6000.0, 6500.0, 7000.0, 7500.0, 8000.0, 8500.0, 9000.0, 9500.0, 10000.0])
 
 #planks equation FUV
 
-a= 2.0*h*c**2
+a= 2.0*h*c
 
 b = h*c/(lfuv*k*t)
 
-blackbodyf = a/((lfuv**5) * (np.exp(b) -1.0))
+blackbodyf = a/((lfuv**3) * (np.exp(b) -1.0))
 
 #planks equation NUV
 
-a2= 2.0*h*c**2
+a2= 2.0*h*c
 
 b2 = h*c/(lnuv*k*t)
 
-blackbodyn = a2/((lnuv**5) * (np.exp(b2) -1.0))
+blackbodyn = a2/((lnuv**3) * (np.exp(b2) -1.0))
 
 #FUV/NUV ratio
 
@@ -55,10 +62,10 @@ plt.plot(t, ratio)
 plt.ylabel('FUV/NUV')
 plt.xlabel('Temperature (K)')
 plt.title('FUV/NUV flux ratio vs. Temperature (K)')
-plt.ylim(0.0, 1.0)
-plt.xlim(5000.0, 15000.0)
-plt.yticks(np.arange(0.0, 1.0, 0.05))
-plt.xticks(np.arange(5000.0, 15000.0, 1000.0))
+plt.ylim(0, max(ratio))
+plt.xlim(x , y)
+plt.yticks(np.arange(0.0, max(ratio), 0.05))
+plt.xticks(np.arange(x  , y, 1000.0))
 plt.gca().invert_xaxis()
 plt.grid()
 plt.show()
